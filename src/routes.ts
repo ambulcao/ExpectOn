636414a2a35c2df51af36891bbe2b15d3027a1af
Alexandre Bulcao <ambulcao@gmail.com>
2021-10-19 22:46:44 +0100

    Creating profile user

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { GetLast3MessageController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
@@ -12,4 +13,5 @@ router.post("/messages", ensureAuthenticated, new CreateMessageController().hand
 
 router.get("/messages/last3", new GetLast3MessageController().handle);
 
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
 export { router };
