ff232595357c653a209bf142515022b3c32e2aff
Alexandre Bulcao <ambulcao@gmail.com>
2021-10-20 16:34:31 +0100

    Creating a message form and adding the business rules for sending messages through the form

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,16 @@
 import styles from './Sass/App.module.scss';
 import { LoginBox } from './components/LoginBox';
 import { MessageList } from './components/MessageList';
+import { useContext } from 'react';
+import { AuthContext } from './context/Auth';
+import { SendMessageForm } from './components/SendMessageForm';
 
 export function App() {
+  const { user } = useContext(AuthContext); 
   return (
    <main className={styles.contentWrapper}>
      <MessageList/>
-     <LoginBox/>
+     { !! user ? <SendMessageForm /> : <LoginBox/> }
    </main>
   )
 }
